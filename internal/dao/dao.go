505ef489315da7969f4aff5a9d277bbb8d84cf80
Your NameDoctor <you@example.com>
2022-05-16 16:13:24 +0530

    Save1

--- internal/dao/dao.go
+++ internal/dao/dao.go
@@ -5,6 +5,7 @@ import (
 	cachemodels "github.hdfcbank.com/HDFCBANK/mb-microservices-utils/cache-util/models"
 	"github.hdfcbank.com/HDFCBANK/mb-microservices-utils/cache-util/types"
 	"github.hdfcbank.com/HDFCBANK/mb-microservices-utils/logger"
+	"go.uber.org/zap"
 )
 
 type daoClient struct {
@@ -37,18 +38,32 @@ func GetDaoAccessor(logger *logger.Logger) (DaoAccessor, error) {
 
 //user_device_mapping
 
-func (d *daoClient) CheckdeviceID(SetName, deviceID string) (bool, error) {
-
-	data := cachemodels.GetCacheConfig{
-		Key:     deviceID,
-		SetName: SetName,
+func (d *daoClient) CheckdeviceID(SetName, device_ID string) (bool, error) {
+	keyvalue := map[string]interface{}{
+		"deviceID": device_ID,
 	}
-	check, err := d.client.CacheExists(data)
+
+	data := cachemodels.GetCacheQueryConfig{
+		SetName: SetName,
+		KeyVals: keyvalue,
+	}
+	check, err := d.client.FilterCache(data)
 	if err != nil {
-		//need to include multilingual error util
-		//d.logger.Error("failed", zap.Error(err))
+		//need to include multilingual error util in handler
+		d.Error("failed to get key from db", zap.Error(err))
+
 		return false, err
 	}
-
-	return check, err
+	if check == nil {
+		d.Error("no data found", zap.Error(err))
+		return false, err
+	}
+	ret := true
+	return ret, nil
 }
+
+/*type GetCacheQueryConfig struct {
+	SetName string                 `validate:"required"` //set or table name
+	Fields  []string               `validate:"required"`
+	KeyVals map[string]interface{} `validate:"required"` // Key value pair for quering
+}*/
